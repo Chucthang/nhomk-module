c76d7c1e068bd5b76105583a81a486f4a122b0b8
jone-dema <ducle199903@gmail,com>
2019-10-15 14:56:00 +0700

    duc

---
+++ 6/6-content.php
@@ -0,0 +1,275 @@
+
+<div class="type-6">
+	<div class="container">
+		<div class="row">
+			<div class="main">
+				<div class="home-products ">
+					<h2 class="widgettitle">What's in Store</h2>
+					<ul class="products" style="list-style: none">
+
+						<li class="product">
+							<span class="onsale">Sale</span>
+							<div class="product-thumb-wrap">
+								<div class="product-thumb">
+									<a href="" rel="nofollow" data-product_id="" data-product_sku="2123"
+										class="button"
+										style="display: block;">Select options</a>
+									<img width="400" height="400" src="/test/images/4406151406_1_1_1-400x400.jpeg"
+									
+										alt="7215050400_1_1_1" title="" srcset=""
+										sizes="(max-width: 400px) 100vw, 400px"> </a>
+								</div>
+							</div>
+
+							<a href="" class="home-product-title">
+								<h3>Ripped Denim</h3>
+
+								<!--  Display Product Price -->
+								<span class="price">
+
+									<span class="price"><del><span class="amount">$99.00</span></del> <ins><span
+												class="amount">$49.00</span></ins></span>
+								</span>
+							</a>
+						</li> <!--  .product  -->
+
+						<li class="product">
+
+							<div class="product-thumb-wrap">
+								<div class="product-thumb">
+									<a href="" rel="nofollow" data-product_id="" data-product_sku="6512"
+										class="button"
+										style="display: block;">Add to cart</a>
+									<img width="400" height="400" src="/test/images/4406151406_1_1_1-400x400.jpeg"
+										alt="4406151406_1_1_1" title="" srcset=""
+										sizes="(max-width: 400px) 100vw, 400px"> </a>
+								</div>
+							</div>
+
+							<a href="" class="home-product-title">
+								<h3>Medium Blue Skinny Jeans</h3>
+
+								<!--  Display Product Price -->
+								<span class="price">
+
+									<span class="price"><span class="amount">$79.00</span></span>
+								</span>
+							</a>
+						</li> <!--  .product  -->
+
+						<li class="product">
+
+							<div class="product-thumb-wrap">
+								<div class="product-thumb">
+									<a href="" rel="nofollow" data-product_id=""
+										data-product_sku="3241" class="button"
+										style="display: block">Add to cart</a>
+									<img width="400" height="400" src="/test/images/blazer_1-400x400.png"
+										
+										alt="0085224438_1_1_1" title="" srcset=""
+										sizes="(max-width: 400px) 100vw, 400px"> </a>
+								</div>
+							</div>
+
+							<a href="" class="home-product-title">
+								<h3>Neon Touch T-Shirt</h3>
+
+								<!--  Display Product Price -->
+								<span class="price">
+
+									<span class="price"><span class="amount">$49.00</span></span>
+								</span>
+							</a>
+						</li> <!--  .product  -->
+
+						<li class="product">
+
+							<span class="onsale ">Sale</span>
+							<div class="product-thumb-wrap">
+								<div class="product-thumb">
+									<a href="" rel="nofollow" data-product_id="" data-product_sku=""
+										class="button " style="display: block">Add
+										to cart</a>
+									<img width="400" height="400" src="/test/images/4406151406_1_1_1-400x400.jpeg"
+									
+										alt="5644227600_1_1_1" title="" srcset=""
+										sizes="(max-width: 400px) 100vw, 400px"> </a>
+								</div>
+							</div>
+
+							<a href="" class="home-product-title">
+								<h3>Navajo Print Linen T-Shirt</h3>
+
+								<!--  Display Product Price -->
+								<span class="price">
+
+									<span class="price"><del><span class="amount">$69.00</span></del> <ins><span
+												class="amount">$39.00</span></ins></span>
+								</span>
+							</a>
+						</li> <!--  .product  -->
+
+						<li class="product last">
+
+							<div class="product-thumb-wrap">
+								<div class="product-thumb">
+									<a href="" rel="nofollow" data-product_id="" data-product_sku="3455"
+										class="button" style="display: block">Add
+										to cart</a>
+									<img width="400" height="400" src="/test/images/blazer_3-400x400.png"
+										
+										alt="3666220712_1_1_1" title="" srcset=""
+										sizes="(max-width: 400px) 100vw, 400px"> </a>
+								</div>
+							</div>
+
+							<a href="" class="home-product-title">
+								<h3>Top With Grosgrain Straps</h3>
+
+								<!--  Display Product Price -->
+								<span class="price">
+
+									<span class="price"><span class="amount">$49.00</span></span>
+								</span>
+							</a>
+						</li>
+						<div class="clear"></div> <!--  .product  -->
+
+						<li class="product">
+
+							<div class="product-thumb-wrap">
+								<div class="product-thumb">
+									<a href="" rel="nofollow" data-product_id="" data-product_sku="3124"
+										class="button "
+										style="display: block;">Add to cart</a>
+									<img width="400" height="400" src="/test/images/blazer_2-400x400.png"
+										 alt="blazer_1"
+										title="" srcset="" sizes="(max-width: 400px) 100vw, 400px"> </a>
+								</div>
+							</div>
+
+							<a href="" class="home-product-title">
+								<h3>Fantasy Fabric Blazer</h3>
+
+								<!--  Display Product Price -->
+								<span class="price">
+
+									<span class="price"><span class="amount">$149.00</span></span>
+								</span>
+							</a>
+						</li> <!--  .product  -->
+
+						<li class="product">
+
+							<span class="onsale ">Sale</span>
+							<div class="product-thumb-wrap">
+								<div class="product-thumb">
+									<a href="" rel="nofollow" data-product_id="" data-product_sku="2412"
+										class="button " style="display: block">Add
+										to cart</a>
+									<img width="400" height="400" src="/test/images/5644227600_1_1_1-400x400.jpeg"
+										 alt="blazer_2"
+										title="" srcset="" sizes="(max-width: 400px) 100vw, 400px"> </a>
+								</div>
+							</div>
+
+							<a href="" class="home-product-title">
+								<h3>Printed Sequinned Blazer</h3>
+
+								<!--  Display Product Price -->
+								<span class="price">
+
+									<span class="price"><del><span class="amount">$249.00</span></del> <ins><span
+												class="amount">$199.00</span></ins></span>
+								</span>
+							</a>
+						</li> <!--  .product  -->
+
+						<li class="product">
+
+							<div class="product-thumb-wrap">
+								<div class="product-thumb">
+									<a href="" rel="nofollow" data-product_id="" data-product_sku=""
+										class="button" style="display: block">Add
+										to cart</a>
+									<img width="400" height="400" src="/test/images/4406151406_1_1_1-400x400.jpeg"
+										 alt="blazer_3"
+										title="" srcset="" sizes="(max-width: 400px) 100vw, 400px"> </a>
+								</div>
+							</div>
+
+							<a href="" class="home-product-title">
+								<h3>Leather Jacket With Studs</h3>
+
+								<!--  Display Product Price -->
+								<span class="price">
+
+									<span class="price"><span class="amount">$499.00</span></span>
+								</span>
+							</a>
+						</li> <!--  .product  -->
+
+						<li class="product">
+
+							<div class="product-thumb-wrap">
+								<div class="product-thumb">
+									<a href="" rel="nofollow" data-product_id=""
+										data-product_sku="4354" class="button "
+										style="display: block">Add to cart</a>
+									<img width="400" height="400" src="/test/images/3666220712_1_1_1-400x400.jpeg"
+										
+										alt="2469573401_1_1_1" title="" srcset=""
+										sizes="(max-width: 400px) 100vw, 400px"> </a>
+								</div>
+							</div>
+
+							<a href="" class="home-product-title">
+								<h3>Printed Trousers</h3>
+
+								<!--  Display Product Price -->
+								<span class="price">
+
+									<span class="price"><span class="amount">$149.00</span></span>
+								</span>
+							</a>
+						</li> <!--  .product  -->
+
+						<li class="product last">
+
+							<span class="onsale">Sale</span>
+							<div class="product-thumb-wrap">
+								<div class="product-thumb">
+									<a href="" rel="nofollow" data-product_id=""
+										data-product_sku="2314" class="button "
+										style="display:block">Add to cart</a>
+									<img width="400" height="400" src="/test/images/2469573401_1_1_1-400x400.jpeg"
+										
+										alt="2563072400_1_1_1" title="" srcset=""
+										sizes="(max-width: 400px) 100vw, 400px"> </a>
+								</div>
+							</div>
+
+							<a href="" class="home-product-title">
+								<h3>Snake Print Jeans</h3>
+
+								<!--  Display Product Price -->
+								<span class="price">
+
+									<span class="price"><del><span class="amount">$129.00</span></del> <ins><span
+												class="amount">$99.00</span></ins></span>
+								</span>
+							</a>
+						</li>
+						<div class="clear"></div> <!--  .product  -->
+
+
+
+					</ul>
+				</div><!-- .home-products -->
+
+			</div> <!--  end .main  -->
+
+		</div> <!--  end .row  -->
+
+	</div> <!-- .container -->
+</div>
