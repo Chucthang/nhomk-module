5c365be751ded7ac8da3c7fb79efe8a5c9bcc452
nguyenminhcanh <canhminh199x@gmail.com>
2019-10-16 10:50:06 +0700

    4

---
+++ 17/js/17.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.hidecart-icon').click(function(){
+       $(this).parent().toggleClass('close-icon');
+       $(this).toggleClass('close-icon');
+    })
+});
+
