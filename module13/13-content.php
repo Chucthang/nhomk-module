f50c43974eb44479c7bb5e3f0cf415d0cd3c5b65
jone-dema <ducle199903@gmail,com>
2019-10-26 10:47:43 +0700

    new

---
+++ module13/13-content.php
@@ -0,0 +1,142 @@
+<div class="type-13">
+    <div class="container">
+        <main class="l-main">
+            <div class="main row">
+            <div class="contact-below">
+                <div class="entry-contact">
+                    <h2 class="title-contact"> Cart </h2>
+                </div>
+                <article>
+                    <div class="body field">
+                        <div class="woocommerce">
+                            <form action="" method="post">
+
+
+                                <table class="shop_table  cart" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th class="">&nbsp;</th>
+                                            <th class="">&nbsp;</th>
+                                            <th class="product-name">Product</th>
+                                            <th class="">Price</th>
+                                            <th class="product-quantity">Quantity</th>
+                                            <th class="">Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        <tr class="">
+
+                                            <td class="">
+                                                <a href=""
+                                                    class="remove" title="Remove this item" data-product_id="70"
+                                                    data-product_sku="">×</a> </td>
+
+                                            <td class="">
+                                                <a href=""><img
+                                                        width="180" height="180"
+                                                        src="/1118/images/0722308250_1_1_1-138x138.jpeg"
+                                                        class="attachment-shop_thumbnail "
+                                                        alt="poster_2_up"
+                                                        srcset=""
+                                                        sizes="(max-width: 180px) 100vw, 180px"></a> </td>
+
+                                            <td class="product-name" data-title="Product">
+                                                <a href="">Flag T-Shirt</a> </td>
+
+                                            <td class="" data-title="Price">
+                                                <span class="amount">£12.00</span> </td>
+
+                                            <td class="product-quantity" data-title="Quantity">
+                                                <div class="quantity">
+                                                    <input type="number" step="1" min="0" max=""
+                                                        name="cart[7cbbc409ec990f19c78c75bd1e06f215][qty]" value="2"
+                                                        title="Qty" class="input-text qty text" size="4">
+                                                </div>
+                                            </td>
+
+                                            <td class="" data-title="Total">
+                                                <span class="amount">£24.00</span> </td>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="6" class="actions">
+
+                                                <div class="coupon">
+
+                                                    <label for="coupon_code">Coupon:</label>Coupon <input type="text"
+                                                        name="coupon_code" class="input-text" id="coupon_code" value=""
+                                                        placeholder="Coupon code"> <input type="submit" class="button"
+                                                        name="apply_coupon" value="Apply Coupon">
+
+                                                        <input type="submit" class="button" name="update_cart"
+                                                    value="Update Cart">
+
+                                                </div>
+                                                <input type="submit" class="button1" name="update_cart"
+                                                    value="Proceed to Checkout">
+                                                
+
+
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                                <input type="hidden" id="_wpnonce" name="_wpnonce"
+                                                    value="a3d497a332"><input type="hidden" name="_wp_http_referer"
+                                                    value="/factory/cart/">
+                                            </td>
+                                        </tr>
+
+                                    </tbody>
+                                </table>
+
+
+                            </form>
+
+                            <div class="cart-collaterals">
+
+                                <div class="cart_totals ">
+
+
+                                    <h2>Cart Totals</h2>
+
+                                    <table cellspacing="0" class="shop_table ">
+
+                                        <tbody>
+                                            <tr class="cart-subtotal">
+                                                <th>
+                                                    Cart Subtotal</th>
+                                                <td data-title="Subtotal"><span class="amount">£24.00</span></td>
+                                            </tr>
+                                            <tr class="cart-subtotal">
+                                                <th>Shipping and Handling</th>
+                                                <td data-title="Subtotal"><span class="amount">Free Shipping</span></td>
+                                            </tr>
+
+
+
+
+
+                                            <tr class="">
+                                                <th>Order Total</th>
+                                                <td data-title="Total"><strong><span
+                                                            class="amount">£24.00</span></strong> </td>
+                                            </tr>
+
+
+                                        </tbody>
+                                    </table>
+                                    <h2>Calculate Shipping</h2>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </article>
+
+            </div>
+        </main>
+    </div>
+
+</div>
---
+++ module19/19-content.php
@@ -0,0 +1,106 @@
+<div class="type-19">
+    <div class="container">
+        <div class="contact">
+            
+            <div class="breadcum">
+                <span class="icon-home"><i class="fa fa-home"></i> / </span>
+                <span class="txt-home"><a> Sitemap
+                    </a></span>
+            </div>
+            <div class="contact-below">
+                <div class="entry-contact">
+                    <h1 class="title-contact"> Sitemaps </h1>
+                </div>
+
+                <div id="left">
+
+
+<h2 id="authors">Authors</h2>
+<ul>
+    <li><a href="" title="Posts by Michael Web">Michael Web</a></li>
+</ul>
+
+<h2 id="pages">Pages</h2>
+<ul>
+    <li><a href="">Blog Full Width</a></li>
+    <li><a href="">Blog Page with Sidebar</a></li>
+    <li><a href="">Cart</a></li>
+    <li><a href="">Checkout</a>
+        <ul>
+            <li><a href="">Checkout → Pay</a></li>
+            <li><a href="">Order Received</a></li>
+        </ul>
+    </li>
+    <li class="n"><a href="">Checkout</a>
+        <ul>
+            <li><a href="">Checkout → Pay</a></li>
+            <li><a href="">Order Received</a></li>
+        </ul>
+    </li>
+    <li><a href="">Clean Design</a></li>
+    <li><a href="">Contact us</a></li>
+    <li><a href="">E-Commerce</a></li>
+    <li><a href="">Fully Localized</a></li>
+    <li><a href="">Home Products</a></li>
+    <li><a href="">Home+Sidebar</a></li>
+    <li><a href="">Home+Sidebar+Widgets</a></li>
+    <li><a href="">My Account</a>
+        <ul>
+            <li><a href="">Change Password</a></li>
+            <li><a href="">Edit My Address</a></li>
+            <li><a href="">View Order</a></li>
+        </ul>
+    </li>
+    <li>
+        <li><a href="">My Account</a>
+            <ul>
+                <li><a href="">Change Password</a></li>
+                <li><a href="">Edit My Address</a></li>
+                <li><a href="">View Order</a></li>
+            </ul>
+    </li>
+    <li><a href="">Page Full Width</a></li>
+    <li><a href="">Page with Sidebar</a></li>
+    <li><a href="">Portfolio 1 Column</a></li>
+    <li><a href="">Portfolio 2 Column</a></li>
+    <li><a href="">Portfolio 3 Column</a></li>
+    <li><a href="">Sample Page</a></li>
+    <li><a href="">Sample Page</a></li>
+    <li><a href="">Sample Page</a></li>
+    <li><a href="">Sample Page</a></li>
+    <li><a href="">Shop</a></li>
+    <li><a href="">Shop</a></li>
+    <li><a href="">Shortcodes</a></li>
+    <li><a href="">Sitemap</a></li>
+    <li><a href="">Theme Options</a></li>
+    <li><a href="">Track your order</a></li>
+    <li><a href="">Track your order</a></li>
+    <li><a href="">Widgetized Homepage</a></li>
+</ul>
+
+<h2 id="posts">Posts</h2>
+<ul>
+    <li>
+        <h3>Blog</h3>
+        <ul>
+            <li><a href="">Specials deals for him on all items</a></li>
+            <li>
+                <a href="">Awesome new shoes</a></li>
+            <li>
+                <a href="">New designer collection</a></li>
+            <li>
+                <a href="">Buy today and get Free Shipping on selected items</a></li>
+            <li>
+                <a href="">Sales &amp; Specials</a></li>
+        </ul>
+    </li>
+</ul>
+</div>
+              
+
+
+            </div>
+        </div>
+    </div>
+</div>
+</div>
