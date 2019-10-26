122c76318746fb5a4164c3b2493f8a18c3a0787e
ChucThang <tructhang2408@gmail.com>
2019-10-26 08:12:10 +0700

    del

--- 17/js/17.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.hidecart-icon').click(function(){
-       $(this).parent().toggleClass('close-icon');
-       $(this).toggleClass('close-icon');
-    })
-});
-
