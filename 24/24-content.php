a8c6325968a51af6bc00cb8498eebe57d01abbe9
hoangnhata2 <33858680+hoangnhata2@users.noreply.github.com>
2019-11-07 14:46:59 +0700

    module-24

---
+++ 24/24-content.php
@@ -0,0 +1,69 @@
+<div class="type-24">
+    <div class="container">
+        <div class="ufo-shortcode box">
+            <h4>Content Box</h4>
+            <p>Mauris luctus lacinia tempus. Aliquam interdum elit eget magna molestie molestie. Cum sociis natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras vestibulum risus nec erat dictum
+                scelerisque porta augue fermentum. Duis vel viverra eros. Donec at purus purus. Mauris id diam et nunc
+                elementum malesuada.</p>
+        </div>
+        <br>
+        <span><a href="javascript:void(0)" class="ufo-code-toggle">+ See The Code</a>
+            <div class="ufo-shortcode code" style="display: none;" id="abc">[half] Content [/half] [half_last]
+                Content [/half_last]</div>
+            <hr class="ufo-shortcode">
+        </span>
+        <p></p>
+        <h4>Highlighted Text</h4>
+        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. <span class="ufo-shortcode hilite">Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum.</span>Lorem ipsum dolor sit amet, consectetur’s
+            adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <div class="hightlight"><a href="javascript:void(0)" class="ufo-code-toggle">+ See The Code</a>
+            <div class="ufo-shortcode code" style="display: none;" id="abc2">[half] Content [/half] [half_last]
+                Content [/half_last]</div>
+            <hr class="ufo-shortcode">
+        </div>
+        <h4>Divider Line</h4>
+        <p></p>
+        <div class="didiver">
+            <a href="javascript:void(0)" class="ufo-code-toggle"> + See The Code </a>
+            <div class="ufo-shortcode code" style="display: none;" id="abc3">[half] Content [/half] [half_last]
+                Content [/half_last]</div>
+            <hr class="ufo-shortcode">
+        </div>
+        <p></p>
+        <h4>Buttons</h4>
+        <p><a href="javascript:void(0)" class="ufo-shortcode button   ">Button</a><a href="javascript:void(0)"
+                class="ufo-shortcode button green  ">Green Button</a><a href="javascript:void(0)"
+                class="ufo-shortcode button blue  ">Blue Button</a><a href="javascript:void(0)"
+                class="ufo-shortcode button red  ">Red Button</a><a href="javascript:void(0)"
+                class="ufo-shortcode button black  ">Black Button</a><a href="javascript:void(0)"
+                class="ufo-shortcode button purple  ">Purple Button</a></p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <li style="list-style: none;"><a href="javascript:void(0)" class="ufo-code-toggle">+ See The Code</a>
+            <div class="ufo-shortcode code" style="display: none;" id="abc4">[half] Content [/half] [half_last]
+                Content [/half_last]</div>
+            <hr class="ufo-shortcode">
+        </li>
+        <h4>Large Buttons</h4>
+        <p><a href="javascript:void(0)" class="ufo-shortcode button  large ">Button</a><a href="javascript:void(0)"
+                class="ufo-shortcode button green large ">Green Button</a><a href="javascript:void(0)"
+                class="ufo-shortcode button blue large ">Blue Button</a><a href="javascript:void(0)"
+                class="ufo-shortcode button red large ">Red Button</a><a href="javascript:void(0)"
+                class="ufo-shortcode button black large ">Black Button</a><a href="javascript:void(0)"
+                class="ufo-shortcode button purple large ">Purple Button</a></p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <div class="button-large" style="list-style: none;"><a href="javascript:void(0)" class="ufo-code-toggle">+ See
+                The Code</a>
+            <div class="ufo-shortcode code" style="display: none;" id="abc5">[half] Content [/half] [half_last]
+                Content [/half_last]</div>
+            <hr class="ufo-shortcode">
+        </div>
+    </div>
+</div>
---
+++ 24/js/24.js
@@ -0,0 +1,25 @@
+    $(document).ready(function() {
+        $("span").click(function() {
+            $("#abc").toggle();
+        })
+    })
+    $(document).ready(function() {
+        $(".hightlight").click(function() {
+            $("#abc2").toggle();
+        })
+    })
+    $(document).ready(function() {
+        $(".didiver").click(function() {
+            $("#abc3").toggle();
+        })
+    })
+    $(document).ready(function() {
+        $("li").click(function() {
+            $("#abc4").toggle();
+        })
+    })
+    $(document).ready(function() {
+        $(".button-large").click(function() {
+            $("#abc5").toggle();
+        })
+    })
